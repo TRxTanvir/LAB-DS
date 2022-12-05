3c535ab82d2a8890e7c2cef219c8593a246fd9bf
Tanvir Hossain <109126803+TRxTanvir@users.noreply.github.com>
2022-12-05 09:59:02 +0600

    Add files via upload

---
+++ lab_4_task_2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node
+{
+	int data;
+	Node* left, * right;
+};
+
+Node* newNode(int data)
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->left = node->right = NULL;
+	return (node);
+}
+
+Node* insertLevelOrder(int arr[],
+					int i, int n)
+{
+	Node *root = nullptr;
+	if (i < n)
+	{
+		root = newNode(arr[i]);
+
+		root->left = insertLevelOrder(arr,
+				2 * i + 1, n);
+		root->right = insertLevelOrder(arr,
+				2 * i + 2, n);
+	}
+	return root;
+}
+
+void inOrder(Node* root)
+{
+	if (root != NULL)
+	{
+		inOrder(root->left);
+		cout << root->data <<" ";
+		inOrder(root->right);
+	}
+}
+int main()
+{
+	int arr[] = { 15, 5, 3, 4, 5, 6, 6, 6, 6 };
+	int n = sizeof(arr)/sizeof(arr[0]);
+	Node* root = insertLevelOrder(arr, 0, n);
+	inOrder(root);
+}
+
+
